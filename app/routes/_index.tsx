ee8c974980ff7211964043b455e941d19d37b508
Oliver Otchere <oliverotchere4@gmail.com>
2024-03-23 10:34:09 +0000

    home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,17 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "pod.link | Share a Podcast" },
+    { name: "description", content: "Welcome to PodLink!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="w-screen min-h-screen bg-blue-500 focus-within:bg-[#db70fc] relative">
+      <div>
+        <p className="font-black text-lg">pod.link</p>
+      </div>
     </div>
   );
 }
